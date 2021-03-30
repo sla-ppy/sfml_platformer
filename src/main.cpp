bccdd9949e27a599baa9597bb78bb1da7b8b1488
S4kyt <jerikolo1@gmail.com>
2021-03-30 22:22:07 +0200

    basic renderwindow

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include <sfml/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 600;
 
-    sf::Window window(sf::VideoMode(screenWidth, screenHeight), "My window");
+    sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "My window");
 
     // limit framerate to 60
     window.setFramerateLimit(60);
@@ -13,6 +13,7 @@ int main()
     // run the program as long as the window is open
     while (window.isOpen())
     {
+
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
@@ -21,6 +22,7 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
     }
 
     return 0;
